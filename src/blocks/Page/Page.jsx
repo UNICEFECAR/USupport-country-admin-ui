ccecde411bb96b981823566d9ad8cae59497a888
Sebastian Galustyan <sebastian@7digit.io>
2022-11-09 18:02:42 +0000

    Add: pass i18n to Navbar component so that languages are displayed.

--- src/blocks/Page/Page.jsx
+++ src/blocks/Page/Page.jsx
@@ -22,7 +22,7 @@ export const Page = ({
   classes,
   children,
 }) => {
-  const { t } = useTranslation("page");
+  const { t, i18n } = useTranslation("page");
   const pages = [
     { name: t("page_1"), url: "/", exact: true },
     { name: t("page_2"), url: "/providers" },
@@ -38,6 +38,7 @@ export const Page = ({
           pages={pages}
           showProfile
           yourProfileText={t("your_profile_text")}
+          i18n={i18n}
         />
       )}
       <div
