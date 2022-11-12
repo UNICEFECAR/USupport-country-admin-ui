871ed269db8a28e934884477a0298e04a10de35d
Sebastian Galustyan <sebastian@7digit.io>
2022-11-12 23:00:08 +0000

    Create: SOSCenter block

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,3 +1,4 @@
 export * from "./Page";
 export * from "./Articles";
 export * from "./FAQ";
+export * from "./SOSCenter";
--- src/blocks/locales.js
+++ src/blocks/locales.js
@@ -1,3 +1,4 @@
 export * as Page from "./Page/locales.js";
 export * as Articles from "./Articles/locales.js";
 export * as FAQ from "./FAQ/locales.js";
+export * as SOSCenter from "./SOSCenter/locales.js";
