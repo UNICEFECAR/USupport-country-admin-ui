a4fabdf310d7c368cdfc481a70f1acb6a0625c7a
Sebastian Galustyan <sebastian@7digit.io>
2022-11-12 22:58:33 +0000

    Create: FAQ block

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,2 +1,3 @@
 export * from "./Page";
 export * from "./Articles";
+export * from "./FAQ";
--- src/blocks/locales.js
+++ src/blocks/locales.js
@@ -1,2 +1,3 @@
 export * as Page from "./Page/locales.js";
 export * as Articles from "./Articles/locales.js";
+export * as FAQ from "./FAQ/locales.js";
