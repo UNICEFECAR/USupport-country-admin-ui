5d4244b846739ea73c395f2a85e8b6ad402cc520
Sebastian Galustyan <sebastian@7digit.io>
2022-11-13 04:33:13 +0000

    Add: locales to Dashboard Page

--- src/pages/locales.js
+++ src/pages/locales.js
@@ -2,3 +2,4 @@ export * as NotFound from "./NotFound/locales.js";
 export * as Articles from "./Articles/locales.js";
 export * as FAQ from "./FAQ/locales.js";
 export * as SOSCenter from "./SOSCenter/locales.js";
+export * as Dashboard from "./Dashboard/locales.js";
