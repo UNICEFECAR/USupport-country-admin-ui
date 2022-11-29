0bbebc32a4890b5212ddf32b8cdabd4699489db4
Georgi 7DIGIT <georgi@7digit.io>
2022-11-29 04:23:37 +0200

    Create: EditProvider page

---
+++ src/pages/EditProvider/EditProvider.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Page, EditProvider as EditProvierBlock } from "#blocks";
+import { UploadPicture, DeleteProfilePicture } from "#backdrops";
+
+import "./edit-provider.scss";
+
+/**
+ * EditProvider
+ *
+ * Edit provider profile
+ *
+ * @returns {JSX.Element}
+ */
+export const EditProvider = () => {
+  const navigate = useNavigate();
+  const providerId = new URLSearchParams(window.location.search).get("id");
+  if (!providerId) return <Navigate to="/providers" />;
+
+  const [providerImage, setProviderImage] = useState();
+  const [providerImageUrl, setProviderImageUrl] = useState();
+
+  const [isUploadPictureBackdropOpen, setIsUploadPictureBackdropOpen] =
+    useState(false);
+  const [isDeletePictureBackdropShown, setIsDeletePictureBackdropShown] =
+    useState(false);
+
+  const openUploadPictureBackdrop = () => setIsUploadPictureBackdropOpen(true);
+  const openDeletePictureBackdrop = () => setIsDeletePictureBackdropShown(true);
+
+  const closeUploadPictureBackdrop = () =>
+    setIsUploadPictureBackdropOpen(false);
+  const closeDeletePictureBackdrop = () =>
+    setIsDeletePictureBackdropShown(false);
+
+  const handleGoBack = () => navigate(-1);
+
+  return (
+    <Page classes="page__edit-provider" handleGoBack={handleGoBack}>
+      <EditProvierBlock
+        {...{
+          openUploadPictureBackdrop,
+          openDeletePictureBackdrop,
+          providerId,
+          providerImage,
+          setProviderImage,
+          providerImageUrl,
+        }}
+      />
+
+      <UploadPicture
+        isOpen={isUploadPictureBackdropOpen}
+        onClose={closeUploadPictureBackdrop}
+        providerImage={providerImage}
+        setProviderImage={setProviderImage}
+        setProviderImageUrl={setProviderImageUrl}
+        providerId={providerId}
+      />
+      <DeleteProfilePicture
+        isOpen={isDeletePictureBackdropShown}
+        onClose={closeDeletePictureBackdrop}
+        providerId={providerId}
+      />
+    </Page>
+  );
+};
