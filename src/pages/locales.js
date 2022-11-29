e70428a484e3631920b1bd927ce234a6f4b5ca02
Georgi 7DIGIT <georgi@7digit.io>
2022-11-29 04:23:50 +0200

    Create: CreateProvider page

--- src/pages/locales.js
+++ src/pages/locales.js
@@ -10,3 +10,7 @@ export * as ResetPassword from "./ResetPassword/locales.js";
 export * as AdminProfile from "./AdminProfile/locales.js";
 export * as EditProfileDetails from "./EditProfileDetails/locales.js";
 export * as ArticleInformation from "./ArticleInformation/locales.js";
+export * as Providers from "./Providers/locales.js";
+export * as EditProvider from "./EditProvider/locales.js";
+export * as CreateProvider from "./CreateProvider/locales.js";
+export * as ProviderOverview from "./ProviderOverview/locales.js";
