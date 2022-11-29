7ff68047ba4f0e753a2d9ef793eb19f46d6081ca
Georgi 7DIGIT <georgi@7digit.io>
2022-11-29 04:24:38 +0200

    Create: hooks to GET and Update providers data

---
+++ src/hooks/useGetProviderData.js
@@ -0,0 +1,51 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { providerSvc } from "@USupport-components-library/services";
+
+/**
+ * Reuseable hook to get and transform the provider data in a desired format
+ */
+export default function useGetProviderData(id = null) {
+  const [providersData, setProvidersData] = useState();
+  const fetchProvidersData = async () => {
+    const { data } = await providerSvc.getProviderByIdAsAdmin(id);
+
+    const formattedData = {
+      providerDetailId: data.provider_detail_id || "",
+      name: data.name || "",
+      patronym: data.patronym || "",
+      surname: data.surname || "",
+      nickname: data.nickname || "",
+      email: data.email || "",
+      phonePrefix: data.phone_prefix || "",
+      phone: data.phone || "",
+      image: data.image || "default",
+      specializations: data.specializations || [],
+      street: data.street || "",
+      city: data.city || "",
+      postcode: data.postcode || "",
+      education: data.education || [],
+      sex: data.sex || "",
+      consultationPrice: data.consultation_price || 0,
+      description: data.description || "",
+      languages: data.languages || [],
+      workWith: data.work_with || [],
+      totalConsultations: data.total_consultations || 0,
+      earliestAvailableSlot: data.earliest_available_slot || "",
+    };
+    return formattedData;
+  };
+
+  const providersDataQuery = useQuery(["provider-data"], fetchProvidersData, {
+    onSuccess: (data) => {
+      const dataCopy = JSON.parse(JSON.stringify(data));
+      setProvidersData({ ...dataCopy });
+    },
+    onError: (err) => console.log(err, "err"),
+    notifyOnChangeProps: ["data"],
+  });
+
+  return [providersDataQuery, providersData, setProvidersData];
+}
+
+export { useGetProviderData };
