0bb215f702191fb3015c337c8f18fdad0fa8b9d0
Georgi 7DIGIT <georgi@7digit.io>
2023-02-07 17:16:54 +0200

    Refactor: connect the SponsorDetails screen to the API

--- src/pages/SponsorDetails/SponsorDetails.jsx
+++ src/pages/SponsorDetails/SponsorDetails.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
+import { Loading } from "@USupport-components-library/src";
+
 import { Page, SponsorDetails as SponsorDetailsBlock } from "#blocks";
+
+import { useGetSponsorData } from "#hooks";
+
+const AMAZON_S3_BUCKET = `${import.meta.env.VITE_AMAZON_S3_BUCKET}`;
 
 import "./sponsor-details.scss";
 
@@ -20,48 +26,20 @@ export const SponsorDetails = () => {
 
   if (!sponsorId) return <Navigate to="/campaigns" />;
 
-  const sponsorData = {
-    sponsorId: "1",
-    sponsorName: "Unicef",
-    email: "mail@user.com",
-    campaigns: 10,
-    activeCampaigns: 2,
-    phoneNumber: "0893948223",
-    campaignsData: [
-      {
-        campaignName: "Unicef1",
-        usedCoupons: 5,
-        totalCoupons: 10,
-        totalPrice: 500,
-        maxCouponsPerUser: 5,
-        startDate: "2020-02-01",
-        endDate: "2020-02-20",
-        status: "active",
-        campaignId: "790",
-        price: 50,
-      },
-      {
-        campaignName: "Unicef2",
-        usedCoupons: 5,
-        totalCoupons: 10,
-        totalPrice: 500,
-        maxCouponsPerUser: 5,
-        startDate: "2020-01-01",
-        endDate: "2020-01-10",
-        status: "inactive",
-        campaignId: "791",
-        price: 50,
-      },
-    ],
-  };
+  const { data: sponsorData, isLoading } = useGetSponsorData(sponsorId);
 
   return (
     <Page
       handleGoBack={() => navigate(-1)}
-      heading={sponsorData.sponsorName}
+      heading={sponsorData?.sponsorName}
+      image={
+        sponsorData?.image
+          ? `${AMAZON_S3_BUCKET}/${sponsorData.image || "default"}`
+          : null
+      }
       classes="page__sponsor-details"
     >
-      <SponsorDetailsBlock data={sponsorData} />
+      {isLoading ? <Loading /> : <SponsorDetailsBlock data={sponsorData} />}
     </Page>
   );
 };
