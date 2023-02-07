4eb1b641e974288bcebc5cb0d291a785b63f0eb7
Georgi 7DIGIT <georgi@7digit.io>
2023-02-07 17:16:01 +0200

    Refactor: connect the Campaigns screen to the API

--- src/blocks/Campaigns/Campaigns.jsx
+++ src/blocks/Campaigns/Campaigns.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
@@ -9,7 +9,10 @@ import {
   Grid,
   GridItem,
   InputSearch,
+  Loading,
 } from "@USupport-components-library/src";
+
+import { useGetAllSponsorsData } from "#hooks";
 
 const AMAZON_S3_BUCKET = `${import.meta.env.VITE_AMAZON_S3_BUCKET}`;
 
@@ -33,54 +36,43 @@ export const Campaigns = () => {
     t("phone"),
   ];
 
-  const data = [
-    {
-      sponsor: "unicef 1",
-      campaigns: 10,
-      activeCampaigns: 2,
-      email: "user@mail.com",
-      phone: "089999999",
-      sponsorId: "1",
-    },
-    {
-      sponsor: "unicef 2",
-      campaigns: 20,
-      activeCampaigns: 4,
-      email: "user1@mail.com",
-      phone: "089999999",
-      sponsorId: "2",
-    },
-    {
-      sponsor: "unicef 3",
-      campaigns: 5,
-      activeCampaigns: 6,
-      email: "user2@mail.com",
-      phone: "089999999",
-      sponsorId: "3",
-    },
-  ];
+  const [searchValue, setSearchValue] = useState("");
 
-  const rowsData = data.map((item, index) => {
-    return [
-      <div key={"item" + index} className="campaigns__sponsor">
-        <img
-          className="campaigns__sponsor__image"
-          src={AMAZON_S3_BUCKET + "/default"}
-        />
-        <p className="text campaigns__sponsor__name">{item.sponsor}</p>
-      </div>,
-      <p>{item.campaigns}</p>,
-      <p>{item.activeCampaigns}</p>,
-      <p>{item.email}</p>,
-      <p>{item.phone}</p>,
-    ];
-  });
+  const { data, isLoading } = useGetAllSponsorsData();
+
+  const rowsData = data
+    ?.filter((x) =>
+      x.sponsorName.toLowerCase().includes(searchValue.toLowerCase())
+    )
+    .map((item, index) => {
+      console.log(item);
+      return [
+        <div key={"item" + index} className="campaigns__sponsor">
+          <img
+            className="campaigns__sponsor__image"
+            src={AMAZON_S3_BUCKET + (`/${item.image}` || "default")}
+          />
+          <p className="text campaigns__sponsor__name">{item.sponsorName}</p>
+        </div>,
+        <p>{item.totalCampaigns}</p>,
+        <p>{item.activeCampaigns}</p>,
+        <p>{item.email}</p>,
+        <p>
+          {item.phonePrefix} {item.phone}
+        </p>,
+      ];
+    });
 
   const menuOptions = [
     {
+      icon: "view",
+      text: t("view"),
+      handleClick: (id) => navigate(`/sponsor-details?sponsorId=${id}`),
+    },
+    {
       icon: "edit",
       text: t("edit"),
-      handleClick: (id) => navigate(`/sponsor-details?sponsorId=${id}`),
+      handleClick: (id) => navigate(`/edit-sponsor?sponsorId=${id}`),
     },
   ];
 
@@ -98,31 +90,58 @@ export const Campaigns = () => {
       <Grid classes="campaigns__information">
         <GridItem xs={4} md={2} lg={3}>
           <p>
-            {t("sponsors")}: <strong>{"10"}</strong>
+            {t("sponsors")}: <strong>{data?.length}</strong>
           </p>
         </GridItem>
         <GridItem xs={4} md={2} lg={3}>
           <p>
-            {t("campaigns")}: <strong>{"2"}</strong>
+            {t("campaigns")}:{" "}
+            <strong>
+              {data
+                ?.filter(
+                  (x) => Number(x.totalCampaigns) && Number(x.activeCampaigns)
+                )
+                .reduce(
+                  (a, b) => Number(a.totalCampaigns) + Number(b.totalCampaigns)
+                )}
+            </strong>
           </p>
         </GridItem>
         <GridItem xs={4} md={2} lg={3}>
           <p>
-            {t("active_campaigns")}: <strong>{"5"}</strong>
+            {t("active_campaigns")}:{" "}
+            <strong>
+              {data
+                ?.filter(
+                  (x) => Number(x.totalCampaigns) && Number(x.activeCampaigns)
+                )
+                .reduce(
+                  (a, b) =>
+                    Number(a.activeCampaigns) + Number(b.activeCampaigns)
+                )}
+            </strong>
           </p>
         </GridItem>
         <GridItem xs={4} md={8} lg={3}>
-          <InputSearch placeholder={t("search")} />
+          <InputSearch
+            placeholder={t("search")}
+            value={searchValue}
+            onChange={setSearchValue}
+          />
         </GridItem>
       </Grid>
 
-      <BaseTable
-        data={data}
-        rows={rows}
-        rowsData={rowsData}
-        menuOptions={menuOptions}
-        handleClickPropName={"sponsorId"}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <BaseTable
+          data={data}
+          rows={rows}
+          rowsData={rowsData}
+          menuOptions={menuOptions}
+          handleClickPropName={"sponsorId"}
+        />
+      )}
     </Block>
   );
 };
---
+++ src/hooks/useGetAllSponsorsData.js
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { adminSvc } from "@USupport-components-library/services";
+
+export const useGetAllSponsorsData = () => {
+  const getAllSponsorsData = async () => {
+    const { data } = await adminSvc.getAllSponsorsData();
+    return data.map((sponsor) => {
+      return {
+        sponsorId: sponsor.sponsor_id,
+        sponsorName: sponsor.name,
+        email: sponsor.email,
+        phonePrefix: sponsor.phone_prefix,
+        phone: sponsor.phone,
+        image: sponsor.image,
+        activeCampaigns: sponsor.active_campaigns,
+        totalCampaigns: sponsor.total_campaigns,
+      };
+    });
+  };
+
+  const getAllSponsorsDataQuery = useQuery(
+    ["sponsor-data"],
+    getAllSponsorsData
+  );
+
+  return getAllSponsorsDataQuery;
+};
