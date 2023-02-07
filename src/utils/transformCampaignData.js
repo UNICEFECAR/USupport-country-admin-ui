45328210294cc8352b5d87cc5646f93d2c372fb8
Georgi 7DIGIT <georgi@7digit.io>
2023-02-07 17:16:17 +0200

    Create: transformCampaignData util

---
+++ src/utils/transformCampaignData.js
@@ -0,0 +1,26 @@
+export const transformCampaignData = (campaign) => {
+  const checkIsCampaignActive = (campaign) => {
+    const now = new Date().getTime();
+    const startDate = new Date(campaign.start_date).getTime();
+    const endDate = new Date(campaign.end_date).getTime();
+
+    return startDate >= now <= endDate && campaign.active;
+  };
+
+  return {
+    couponCode: campaign.coupon_code,
+    campaignId: campaign.campaign_id,
+    name: campaign.name,
+    budget: campaign.budget,
+    numberOfCoupons: campaign.no_coupons,
+    couponPrice: campaign.price_per_coupon,
+    maxCouponsPerClient: campaign.max_coupons_per_client,
+    startDate: new Date(campaign.start_date),
+    endDate: new Date(campaign.end_date),
+    termsAndConditions: campaign.terms_and_conditions,
+    couponData: campaign.coupons,
+    usedCoupons: campaign.coupons?.length || 0,
+    usedBudget: (campaign.coupons?.length || 0) * campaign.price_per_coupon,
+    active: checkIsCampaignActive(campaign),
+  };
+};
