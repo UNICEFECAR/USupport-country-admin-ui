b48786229e54002d6ef2da76f7f291e05aad6b45
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2024-04-11 11:23:04 +0300

    Add: new questions to the SecurityCheck

--- src/hooks/useGetSecurityChecks.js
+++ src/hooks/useGetSecurityChecks.js
@@ -9,11 +9,17 @@ export const useGetSecurityChecks = () => {
       const providerData = item.providerData;
       const finalItem = {
         answers: {
+          providerAttend: item.provider_attend,
           contactsDisclosure: item.contacts_disclosure,
           suggestOutsideMeeting: item.suggest_outside_meeting,
           identityCoercion: item.identity_coercion,
           unsafeFeeling: item.unsafe_feeling,
           moreDetails: item.more_details,
+          feeling: item.feeling,
+          addressedNeeds: item.addressed_needs,
+          improveWellbeing: item.improve_wellbeing,
+          feelingsNow: item.feelings_now,
+          additionalComment: item.additional_comment,
         },
         providerName: `${providerData.name} ${providerData.patronym} ${providerData.surname}`,
         providerDetailId: item.provider_detail_id,
