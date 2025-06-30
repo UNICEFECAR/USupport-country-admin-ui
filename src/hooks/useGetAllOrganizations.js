051e0e435fbb2563c4a95531300034cbf871f736
7Digit-main <it-support@7digit.io>
2025-06-30 11:16:51 +0300

    Refactor: organizations fetching

--- src/hooks/useGetAllOrganizations.js
+++ src/hooks/useGetAllOrganizations.js
@@ -4,9 +4,23 @@ import { organizationSvc } from "@USupport-components-library/services";
 export default function useGetAllOrganizations() {
   return useQuery(["organizations"], async () => {
     const data = await organizationSvc.getAllOrganizations();
+
     return data.map((x) => ({
       ...x,
       organizationId: x.organization_id,
+      unitName: x.unit_name,
+      websiteUrl: x.website_url,
+      address: x.address,
+      phone: x.phone,
+      email: x.email,
+      description: x.description,
+      workWith: x.work_with,
+      location: {
+        longitude: x?.longitude,
+        latitude: x?.latitude,
+      },
+      district: x.district_id,
+      specialisations: x.specialisations,
     }));
   });
 }
--- src/hooks/useGetOrganizationsWithDetails.js
+++ src/hooks/useGetOrganizationsWithDetails.js
@@ -6,11 +6,40 @@ export const useGetOrganizationsWithDetails = () => {
     queryKey: ["GetOrganizationsWithDetails"],
     queryFn: async () => {
       const data = await organizationSvc.getOrganizationsWithDetails();
+
       return data.map((x) => ({
-        ...x,
         organizationId: x.organization_id,
+        name: x.name,
+        unitName: x.unit_name,
+        websiteUrl: x.website_url,
+        address: x.address,
+        phone: x.phone,
+        email: x.email,
+        description: x.description,
+        createdBy: x.created_by,
+        createdAt: x.created_at,
+        location: {
+          longitude: x?.longitude,
+          latitude: x?.latitude,
+        },
+        district: {
+          id: x?.district_id,
+          name: x?.district,
+        },
+        paymentMethod: {
+          id: x?.payment_method_id,
+          name: x?.payment_method,
+        },
+        userInteraction: {
+          id: x?.user_interaction_id,
+          name: x?.user_interaction,
+        },
+        workWith: x?.work_with || [],
+        providers: x?.providers || [],
+        specialisations: x?.specialisations || [],
       }));
     },
   });
 };
+
 export default useGetOrganizationsWithDetails;
